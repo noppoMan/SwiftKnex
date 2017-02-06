c805456a4e6379c802f696d7d0a2d4b64e45f903
noppoman <yuki@miketokyo.com>
2017-02-07 02:11:12 +0900

    renew install-mysql-5.7.sh

--- Scripts/install-mysql-5.7.sh
+++ Scripts/install-mysql-5.7.sh
@@ -1,10 +1,22 @@
-sudo apt-get remove --purge "^mysql.*"
-sudo apt-get autoremove
-sudo apt-get autoclean
-sudo rm -rf /var/lib/mysql
-sudo rm -rf /var/log/mysql
-echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
-wget http://dev.mysql.com/get/mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
-sudo dpkg --install mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
-sudo apt-get update -q
-sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server
+#!/bin/bash
+set -x
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+curl -LO https://dev.mysql.com/get/mysql-apt-config_0.5.3-1_all.deb
+echo mysql-apt-config mysql-apt-config/select-product          select Apply              | sudo debconf-set-selections
+echo mysql-apt-config mysql-apt-config/select-server           select mysql-5.7          | sudo debconf-set-selections
+echo mysql-apt-config mysql-apt-config/select-connector-python select none               | sudo debconf-set-selections
+echo mysql-apt-config mysql-apt-config/select-workbench        select none               | sudo debconf-set-selections
+echo mysql-apt-config mysql-apt-config/select-utilities        select none               | sudo debconf-set-selections
+echo mysql-apt-config mysql-apt-config/select-connector-odbc   select connector-odbc-x.x | sudo debconf-set-selections
+sudo -E dpkg -i mysql-apt-config_0.5.3-1_all.deb
+sudo apt-get update
+echo mysql-community-server mysql-community-server/re-root-pass password ${mysql_root_password} | sudo debconf-set-selections
+echo mysql-community-server mysql-community-server/root-pass    password ${mysql_root_password} | sudo debconf-set-selections
+sudo -E apt-get -y install mysql-community-server
+
+echo "Checking installed version....."
+mysql -D mysql -e "SELECT version()"
+echo "Done!!"
