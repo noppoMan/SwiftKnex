5fcfa7f7f91298661fb7755feb5c03baad425272
井川司 <tsukasa.ikawa@secual-inc.com>
2017-12-27 18:14:01 +0900

    modify Package.swift

--- Package.swift
+++ Package.swift
@@ -5,12 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftKnex",
+    products: [
+        .library(name: "SwiftKnex", targets: ["SwiftKnex"]),
+        .executable(name: "SwiftKnexMigration", targets: ["SwiftKnexMigration"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/noppoMan/Prorsum.git", from: "0.3.0")
     ],
     targets: [
         .target(name: "Mysql", dependencies: ["Prorsum"]),
         .target(name: "SwiftKnex", dependencies: ["Mysql"]),
-        .target(name: "SwiftKnexMigration", dependencies: ["SwiftKnex", "Mysql"])
+        .target(name: "SwiftKnexMigration", dependencies: ["SwiftKnex", "Mysql"]),
+        .testTarget(name: "SwiftKnexTests", dependencies: ["SwiftKnex", "Mysql"], path: "Tests"),
     ]
 )
