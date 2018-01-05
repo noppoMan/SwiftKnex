28aa56197be01dd4633be96ced605489627b15a0
tuken <tuken24@icloud.com>
2018-01-05 20:03:52 +0900

    delete "create database swift_knex_test"

--- Scripts/install-mysql-5.7.sh
+++ Scripts/install-mysql-5.7.sh
@@ -13,7 +13,7 @@ set -x
 set -e
 sudo  mysqld_safe --skip-grant-tables &
 sleep 4
-sudo mysql -e "create database swift_knex_test; use mysql; update user set authentication_string=PASSWORD('') where user='root'; update user set plugin='mysql_native_password'; FLUSH PRIVILEGES;"
+sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('') where user='root'; update user set plugin='mysql_native_password'; FLUSH PRIVILEGES;"
 sudo kill -9 `sudo cat /var/lib/mysql/mysqld_safe.pid`
 sudo kill -9 `sudo cat /var/run/mysqld/mysqld.pid`
 sudo service mysql restart
